b5970ab6a3b45d3567f3b97ff114848edfb1ca3d
Rudra Verma <rudraverma781@gmail.com>
2025-12-16 20:16:03 +0530

    New question added !!!

---
+++ basics/age_validate.js
@@ -0,0 +1,7 @@
+let age = 19
+
+if(age > 60) console.log("old man !")
+else if(age > 18) console.log("Adult !!!")
+else if(age>12) console.log("Teenager |||");
+else console.log("child !!");
+
---
+++ commit.bat
@@ -0,0 +1,5 @@
+git add .
+git commit -m " New question added !!!"
+git push -u origin main
+exit()
+
