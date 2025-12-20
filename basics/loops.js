fa2f8756aeccf21b7ea50c8d97f8ab851a207fbd
Rudra Verma <rudraverma781@gmail.com>
2025-12-20 10:52:50 +0530

    New question added !!!

---
+++ basics/loops.js
@@ -0,0 +1,15 @@
+
+for (let i = 0; i < 5; i++) {
+  console.log( "The number is " + i );
+  
+}
+
+
+let colors = ["red", "green", "blue"];
+for (let i = 0; i < colors.length; i++) {
+    console.log( colors[i] );
+}
+
+for (let color of colors) {
+    console.log( color );
+}
